bb7f43a892e46f29c255c9d18f832a8536c3b447
sae-web <sae-web@sina.com.cn>
2016-01-02 15:44:57 +0000

    auto versioning

---
+++ OligoDesigner/OligoDesigner/urls.py
@@ -0,0 +1,17 @@
+from django.conf.urls import patterns, include, url
+from OligoDesigner.views import *
+from django.contrib import admin
+import settings
+admin.autodiscover()
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'OligoDesigner.views.home', name='home'),
+    # url(r'^blog/', include('blog.urls')),
+
+    url(r'^site_medias/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT }),
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', index),
+    url(r'^probedesign', probedesign),
+    url(r'^subseq', subseq),
+)
