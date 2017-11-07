badc4514a1a7ce63979323c9fcf0ee3af083a7e7
lxk@yhkodia.com <lxk@yhkodia.com@4d950dd5-4564-4c36-a370-d57b197188d4>
2017-11-07 03:50:29 +0000

    version 1

--- 1/index.wsgi
+++ 1/index.wsgi
@@ -1,3 +1,13 @@
-def application(environ, start_response):
-    start_response('200 ok', [('content-type', 'text/plain')])
-    return ['Hello, SAE!']
+import sys
+import os.path
+
+os.environ['DJANGO_SETTINGS_MODULE'] = 'OligoDesigner.settings' 
+sys.path.append(os.path.join(os.path.dirname(__file__), 'OligoDesigner'))
+root = os.path.dirname(__file__)
+
+sys.path.insert(0, os.path.join(root, 'site-packages'))
+
+import sae
+import django.core.handlers.wsgi
+
+application = sae.create_wsgi_app(django.core.handlers.wsgi.WSGIHandler())
---
+++ 1/static/js/textareastyle.js
@@ -0,0 +1,18 @@
+//jQuery实现textarea高度根据内容自适应
+        $.fn.extend({
+            txtaAutoHeight: function () {
+                return this.each(function () {
+                    var $this = $(this);
+                    if (!$this.attr('initAttrH')) {
+                        $this.attr('initAttrH', $this.outerHeight());
+                    }
+                    setAutoHeight(this).on('input', function () {
+                        setAutoHeight(this);
+                    });
+                });
+                function setAutoHeight(elem) {
+                    var $obj = $(elem);
+                    return $obj.css({ height: $obj.attr('initAttrH'), 'overflow-y': 'hidden' }).height(elem.scrollHeight);
+                }
+            }
+        });
